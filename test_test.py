80ac61cf487d40d654914bcae5ded3973dac37d9
Alex Syrov <lexxani42@gmail.com>
2021-11-27 20:52:31 +0200

    slpitted assers

--- test_test.py
+++ test_test.py
@@ -1,9 +1,19 @@
 from test import D2ruViewsScrapper
 
+scrapper = D2ruViewsScrapper('login', 'password')
 
-def test_scrapper():
-    scrapper = D2ruViewsScrapper('login', 'password')
+
+def test_lexani42():
     assert scrapper.get_user_profile_views('lexani42') == 393
+
+
+def test_test():
     assert scrapper.get_user_profile_views('test') == 27
+
+
+def test_test_na_dauna():
     assert scrapper.get_user_profile_views('TEST NA DAUNA') == 16
+
+
+def test_chelik():
     assert scrapper.get_user_profile_views('Chelik') == 30
